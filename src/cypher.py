271df7b5413d47cd766fe2550e86591208e29a31
David Gilarranz Ortega <david.gilarranz016@comunidadunir.net>
2025-06-10 19:46:15 +0200

    AESCypher -> decrypts messages

--- src/cypher.py
+++ src/cypher.py
@@ -24,6 +24,23 @@ class AESCypher:
             'iv': b64encode(iv).decode()
         }
 
+    def decrypt(self, cyphertext: str, iv: str) -> str:
+        # Decode the cyphertext and iv
+        raw_cyphertext = b64decode(cyphertext)
+        raw_iv = b64decode(iv)
+
+        # Decrypt the message
+        cypher = AES.new(self.__key, AES.MODE_CBC, raw_iv)
+        plaintext = cypher.decrypt(raw_cyphertext)
+
+        # Unpad and return the response
+        return self.__unpad(plaintext).decode()
+
     def __pad(self, plaintext: str) -> str:
         # Adds PKCS#7 padding -> k - (l mod k) octects with value k - (l mod k)
         return plaintext + (16 - len(plaintext) % 16) * chr(16 - len(plaintext) % 16)
+
+    def __unpad(self, plaintext: bytes) -> bytes:
+        # Reverse the operation -> since the padding value is the number of added bytes,
+        # slice the received plaintext up to the first padded byte
+        return plaintext[:-ord(plaintext[len(plaintext) - 1:])]
--- tests/test_cypher.py
+++ tests/test_cypher.py
@@ -20,6 +20,14 @@ def test_uses_supplied_key_to_encrypt_different_message(mocker: MockFixture) -> 
     message = 'Completely different test message'
     run_encryption_test_scenario(message, mocker)
 
+def test_uses_supplied_key_and_iv_to_decode_encrypted_messages(mocker: MockFixture) -> None:
+    message = 'Test message'
+    run_decryption_test_scenario(message, mocker)
+    
+def test_uses_supplied_key_and_iv_to_decode_a_different_encrypted_messages(mocker: MockFixture) -> None:
+    message = 'Different test message'
+    run_decryption_test_scenario(message, mocker)
+
 ###################################################################################
 #                                                                                 #
 # Test scenarios to avoid test-case code duplication                              #
@@ -44,6 +52,21 @@ def run_encryption_test_scenario(plaintext: str, mocker: MockFixture) -> None:
     # Expect the cypher to produce the same encrypted message and return the same iv
     assert output['body'] == encrypted_message and output['iv'] == base64_encoded_iv
 
+def run_decryption_test_scenario(plaintext: str, mocker: MockFixture) -> None:
+    # Encrypt the message
+    key = secrets.token_bytes(32)
+    cypher = AESCypher(key)
+    encrypted_message = cypher.encrypt(plaintext)
+
+    # Decrypt the message
+    cyphertext = base64.b64decode(encrypted_message['body'])
+    iv = base64.b64decode(encrypted_message['iv'])
+    decrypted_message = decrypt(cyphertext, key, iv)
+
+    # Expect the decrypted message to match the one returned by the cypher
+    assert cypher.decrypt(encrypted_message['body'], encrypted_message['iv']) == decrypted_message
+    
+
 ###################################################################################
 #                                                                                 #
 # Helper functions for test scenarios                                             #
@@ -52,10 +75,20 @@ def run_encryption_test_scenario(plaintext: str, mocker: MockFixture) -> None:
 
 # Helper function to encrypt text
 def encrypt(plaintext: str, key: bytes, iv: bytes) -> str:
-    body = pad(plaintext).encode()
+    padded_plaintext = pad(plaintext).encode()
     cipher = AES.new(key, AES.MODE_CBC, iv)
-    return base64.b64encode(cipher.encrypt(body)).decode()
+    return base64.b64encode(cipher.encrypt(padded_plaintext)).decode()
 
 def pad(plaintext: str) -> str:
     # Adds PKCS#7 padding -> k - (l mod k) octects with value k - (l mod k)
     return plaintext + (16 - len(plaintext) % 16) * chr(16 - len(plaintext) % 16)
+
+def decrypt(cyphertext: bytes, key: bytes, iv: bytes) -> str:
+    cypher = AES.new(key, AES.MODE_CBC, iv)
+    padded_plaintext = cypher.decrypt(cyphertext).decode()
+    return unpad(padded_plaintext)
+
+def unpad(plaintext: str) -> str:
+    # Strip the padding -> Since the padding value is the number of padded bytes,
+    # use its value to end the returned slice
+    return plaintext[:-ord(plaintext[len(plaintext) - 1:])]
