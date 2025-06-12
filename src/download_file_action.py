9a9a17339dfe1e0e60ff5821ee15fdf164f94f69
David Gilarranz Ortega <david.gilarranz016@comunidadunir.net>
2025-06-12 20:17:49 +0200

    DownloadFileAction -> sends request

--- src/download_file_action.py
+++ src/download_file_action.py
@@ -2,4 +2,13 @@ from src.action import Action
 from src.http_service import HTTPService
 
 class DownloadFileAction(Action):
-    pass
+    def run(self, args: dict[str, str]) -> str:
+        # Create and send request
+        request = {
+            'action': 'download_file',
+            'args': {
+                'filename': args['filename']
+            }
+        }
+        response = HTTPService().send_request(request)
+        return ''
--- tests/test_download_file_action.py
+++ tests/test_download_file_action.py
@@ -34,11 +34,35 @@ def http_service(mocker: pytest_mock.MockFixture) -> MagicMock:
 def test_is_action() -> None:
     assert issubclass(DownloadFileAction, Action)
 
+def test_requests_a_file_to_be_downloaded(http_service: MagicMock, fs: FakeFilesystem) -> None:
+    filename = 'test.txt'
+    run_requests_file_test_scenario(filename, http_service)
+
+def test_requests_a_different_file_to_be_downloaded(http_service: MagicMock, fs: FakeFilesystem) -> None:
+    filename = 'different_file.txt'
+    run_requests_file_test_scenario(filename, http_service)
+
 ################################################################################
 #                                                                              #
 # Test scenarios to avoid test-case code duplication                           #
 #                                                                              #
 ################################################################################
+
+def run_requests_file_test_scenario(filename: str, http_service: MagicMock) -> None:
+    # Create request
+    request = {
+        'action': 'download_file',
+        'args': {
+            'filename': filename
+        }
+    }
+    
+    # Run the action
+    action = DownloadFileAction()
+    action.run({ 'filename': filename })
+
+    # Expect the request to have been made
+    http_service.send_request.assert_called_once_with(request)
 
 ################################################################################
 #                                                                              #
