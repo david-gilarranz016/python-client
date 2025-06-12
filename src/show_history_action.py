7328d460e7f911b93350cc2ebbbf2874a6e83328
David Gilarranz Ortega <david.gilarranz016@comunidadunir.net>
2025-06-12 19:23:11 +0200

    ShowHistoryAction -> returns last instance of command when asked

--- src/show_history_action.py
+++ src/show_history_action.py
@@ -3,4 +3,14 @@ from src.history_service import HistoryService
 
 class ShowHistoryAction(Action):
     def run(self, args: dict[str, str]) -> str:
-        return '\n'.join(HistoryService().get_history())
+        # Get a history service instance
+        history_service = HistoryService()
+
+        # Search a command or return the full history
+        output = ''
+        if 'search' in args.keys():
+            output = history_service.search_command(args['search'])[-1]
+        else:
+            output = '\n'.join(HistoryService().get_history())
+
+        return output
--- tests/test_show_history_action.py
+++ tests/test_show_history_action.py
@@ -36,6 +36,20 @@ def test_returns_different_saved_history(history_service: HistoryService, fs: py
     saved_history = [ 'id', 'groups', 'docker ps -a' ]
     run_returns_saved_history_test_scenario(saved_history, history_service)
 
+def test_returns_last_instance_of_executed_command_when_supplied_the_search_parameter(
+        history_service: HistoryService, fs: pyfakefs.fake_filesystem.FakeFilesystem) -> None:
+    target = 'ls'
+    saved_history = [ 'pwd', 'id', 'ls -l /home', 'cd /home/web-admin', 'ls -a' ]
+    run_search_history_test_scenario(saved_history, target, history_service)
+
+def test_returns_last_instance_of_executed_command_when_supplied_different_search_parameter(
+        history_service: HistoryService, fs: pyfakefs.fake_filesystem.FakeFilesystem) -> None:
+    target = 'cd'
+    saved_history = [ 'pwd', 'id', 'ls -l /home', 'cd /home/web-admin', 'ls -a', 'cd .ssh' ]
+    run_search_history_test_scenario(saved_history, target, history_service)
+
+
+
 ################################################################################
 #                                                                              #
 # Test scenarios to avoid test-case code duplication                           #
@@ -55,6 +69,18 @@ def run_returns_saved_history_test_scenario(saved_history: list[str], history_se
     expected_result = '\n'.join(saved_history)
     assert result == expected_result
 
+def run_search_history_test_scenario(saved_history: list[str], cmd: str, history_service: HistoryService) -> None:
+    # Save the list of commands
+    for cmd in saved_history:
+        history_service.add_command(cmd)
+
+    # Run the action
+    action = ShowHistoryAction()
+    result = action.run({ 'search': cmd })
+
+    # Expet the last occurrence of cmd to be returned
+    expected_result = [ c for c in saved_history if c.startswith(cmd) ][-1]
+    assert result == expected_result
 
 ################################################################################
 #                                                                              #
