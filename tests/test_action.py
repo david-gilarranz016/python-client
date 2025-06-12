13a26dffb28287b7f9ca1350c3d7d05c5b2c417a
David Gilarranz Ortega <david.gilarranz016@comunidadunir.net>
2025-06-12 19:10:47 +0200

    Action -> method run is abstract

--- tests/test_action.py
+++ tests/test_action.py
@@ -1,8 +1,17 @@
 from abc import ABC
 from src.action import Action
 
+################################################################################
+#                                                                              #
+# Test cases                                                                   #
+#                                                                              #
+################################################################################
+
 def test_is_abstract_class() -> None:
     assert issubclass(Action, ABC)
 
 def test_has_run_method() -> None:
     assert hasattr(Action, 'run') and callable(getattr(Action, 'run'))
+
+def test_run_method_is_abstract() -> None:
+    assert 'run' in Action.__abstractmethods__
