9c11d24a4bd4fadd06b6920f789ffb2dd5b29efd
David Gilarranz Ortega <david.gilarranz016@comunidadunir.net>
2025-06-13 16:29:27 +0200

    ExecuteCommandAction -> sends request

--- src/execute_command_action.py
+++ src/execute_command_action.py
@@ -1,6 +1,16 @@
 from src.action import Action
+from src.http_service import HTTPService
 from typing import Any
 
 class ExecuteCommandAction(Action):
-    def run(args: dict[str, Any]) -> str:
-        pass
+    def run(self, args: dict[str, Any]) -> str:
+        # Send the request
+        request = {
+            'action': 'execute_command',
+            'args': {
+                'cmd': args['cmd']
+            }
+        }
+        response = HTTPService().send_request(request)
+
+        return ''
--- tests/test_execute_command_action.py
+++ tests/test_execute_command_action.py
@@ -4,12 +4,35 @@ from src.history_service import HistoryService
 from src.http_service import HTTPService
 
 import pytest
+from unittest.mock import MagicMock
 
 ################################################################################
 #                                                                              #
 # Fixtures -> used for setup and teardown                                      #
 #                                                                              #
 ################################################################################
+
+@pytest.fixture
+def http_service() -> MagicMock:
+    # Mock HTTPService to return a mock instance
+    http_service = MagicMock()
+    setattr(HTTPService, 'instance', http_service)
+
+    yield http_service
+
+    # Revert mock
+    delattr(HTTPService, 'instance')
+
+@pytest.fixture
+def history_service() -> MagicMock:
+    # Mock history_service to return a mock instance
+    history_service = MagicMock()
+    setattr(HistoryService, 'instance', history_service)
+
+    yield history_service 
+
+    # Revert mock
+    delattr(HistoryService, 'instance')
 
 
 ################################################################################
@@ -21,12 +44,35 @@ import pytest
 def test_is_action() -> None:
     assert issubclass(ExecuteCommandAction, Action)
 
+def test_uses_http_service_to_request_command(http_service: MagicMock, history_service: MagicMock) -> None:
+    cmd = 'id'
+    run_sends_command_test_scenario(cmd, http_service)
+
+def test_uses_http_service_to_request_different_command(http_service: MagicMock, history_service: MagicMock) -> None:
+    cmd = 'pwd'
+    run_sends_command_test_scenario(cmd, http_service)
 
 ################################################################################
 #                                                                              #
 # Test scenarios to avoid test-case code duplication                           #
 #                                                                              #
 ################################################################################
+
+def run_sends_command_test_scenario(cmd: str, http_service: MagicMock) -> None:
+    # Craft the expected request
+    request = {
+        'action': 'execute_command',
+        'args': {
+            'cmd': cmd
+        }
+    }
+
+    # Run the action
+    action = ExecuteCommandAction()
+    action.run({ 'cmd': cmd })
+
+    # Expect the HTTPService to have been requested the request
+    http_service.send_request.assert_called_once_with(request)
 
 
 ################################################################################
